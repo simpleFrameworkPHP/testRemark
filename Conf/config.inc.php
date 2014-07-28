cb438562bec5f11da899edbe0ce90b0803847bc4
liushaochen <liushaochen1987@qq.com>
2014-07-28 16:20:23 +0800

    代码初设；

---
+++ Conf/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaochen.liu
+ * Date: 14-7-28
+ * Time: 下午3:56
+ */
+return array(
+    'DEFAULT_MODULE'        =>  'Admin',  // 默认模块
+    'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
+    'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+);
