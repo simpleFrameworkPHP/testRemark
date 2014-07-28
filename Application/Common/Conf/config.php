982aadaba1d01c4bc65cf01c85e51bfcf1d2d37b
liushaochen <liushaochen1987@qq.com>
2014-07-28 18:08:17 +0800

    调试后台模板；

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,4 +4,7 @@ return array(
     'DEFAULT_MODULE'        =>  'Admin',  // 默认模块
     'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
     'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+    'TMPL_PARSE_STRING'    => array(
+
+    )
 );
